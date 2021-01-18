9064a32d8982006a8b63725eab48e76869fb7ae1
Nahuel Bergamo <nahuel.h.bergamo@gmail.com>
2021-01-18 00:55:21 -0300

    Added a bunch of things, some hardcoding in favor of quick prototyping

--- app.gd
+++ app.gd
@@ -3,6 +3,14 @@ extends Control
 var read_dir = ''
 var write_dir = ''
 
+# Region
+var usa = 0
+var europe = 0
+var japan = 0
+var asia = 0
+# Licensed?
+var unlicensed = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Menu Items
@@ -38,7 +46,7 @@ func _on_help_pressed(id):
 	var about_name = $MenuButtonHelp.get_popup().get_item_text(id)
 	match about_name:
 		'About':
-			$AboutDialog.popup()	
+			$AboutDialog.popup()
 		'Github Repo':
 			OS.shell_open('https://github.com/nahuelbergamo/RomCleanerEx')
 
@@ -49,5 +57,21 @@ func _on_SaveFilteredDialog_dir_selected(dir):
 
 # To choose a ROMs folder to filter
 func _on_FolderDialog_dir_selected(dir):
+	if $RomList != null:
+		$RomList.clear()
 	print('filter roms from: ' + dir)
 	read_dir = dir
+	var files = []
+	var dir_aux = Directory.new()
+	dir_aux.open(read_dir)
+	dir_aux.list_dir_begin()
+	while true:
+		var file = dir_aux.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			$RomList.add_item(file.get_basename(),null,false)
+			# Also sum up the stats vars
+			if '(USA)' in file.get_basename():
+				usa = usa + 1
+	$StatsReportLabel.text = 'USA Games: ' + str(usa)
